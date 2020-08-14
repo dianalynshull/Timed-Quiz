561740de3580d9ecb8136a64d99c7f95782145ef
dianalynshull <dianalynshull@gmail.com>
2020-08-14 15:55:43 -0600

    simplified html code so that I can test functionality before adding more content. Also added a js file with some pseudocode and a css file with some basic pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Timed-Quiz
+
+quiz questions - https://www.geeksforgeeks.org/javascript-quiz-set-1/
---
+++ script.js
@@ -0,0 +1,15 @@
+// When I click on "Start quiz" I will see the first question and the timer will begin
+// If I answer the question correctly (by clicking on the answer):
+    // I am notified that I got the question correct
+    // The next question is displayed
+// If I answer the question incorrectly (by clickin on the answer):
+    // 10 seconds is removed from the timer
+    // I am notified the question was wrong
+    //  Next question is displayed
+// When all 5 questions are answered:
+    // I am shown a page with my score (based off of how many seconds were left on the timer) 
+    // I am given an option to enter my initials for the high score board
+// When I enter my initials and hit the submit button:
+    // I am shown a page with all of the high scores of the users that have taken the quiz
+    // There is an option to clear the highscore bored
+    // There is an option to play again
