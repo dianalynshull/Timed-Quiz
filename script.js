85c331b5280da05543175ec25e3171e16321437d
dianalynshull <dianalynshull@gmail.com>
2020-08-15 19:57:45 -0600

    created function to create the quiz questions:

--- script.js
+++ script.js
@@ -1,86 +1,115 @@
 // variables
-const quiz = document.getElementById("quiz");
-console.log(quiz);
-const startQuiz = document.getElementById("start");
-console.log(startQuiz);
-const startCard = document.getElementById("startCard");
-console.log(startCard);
-const quizCard = document.getElementById("quizCard");
-console.log(quizCard);
 
-let questionTitle = document.getElementById("question")
-let questions = [
+console.dir(document.body);
+const quiz = document.getElementById("quiz");
+const startBtn = document.getElementById("start");
+const startCard = document.getElementById("startCard");
+const quizCard = document.getElementById("quizCard");
+const questionTitle = document.getElementById("questionTitle");
+const quizChoices = document.getElementById("quizChoices");
+
+// need a variable to define what the current question is
+
+let displayedQuestion = 0;
+let choiceValue = 0;
+
+let questionItem = [
     {
        question: "What is the HTML tag under which one can write the JavaScript code?",
-       choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+       choice: ["<javascript>", "<scripted>", "<script>", "<js>"],
        answer: 2,
         
     },
     {
        question: "Choose the correct JavaScript syntax to change the content of <p id='geek'>GeeksforGeeks</p>",
-       choices: ["document.getElement('geek').innerHTML='I am a Geek';", "document.getElementById('geek').innerHTML='I am a Geek';", "document.getId('geek')='I am a Geek';", "document.getElementById('geek').innerHTML=I am a Geek;"], 
+       choice: ["document.getElement('geek').innerHTML='I am a Geek';", "document.getElementById('geek').innerHTML='I am a Geek';", "document.getId('geek')='I am a Geek';", "document.getElementById('geek').innerHTML=I am a Geek;"], 
        answer: 1,
         
     },
     {
        question: "Which of the following is the correct syntax to display 'GeeksforGeeks' in an alert box using JavaScript?",
-       choices: ["alertbox('GeeksforGeeks');", "msg('GeeksforGeeks');", "msgbox('GeeksforGeeks');", "alert('GeeksforGeeks');"],
+       choice: ["alertbox('GeeksforGeeks');", "msg('GeeksforGeeks');", "msgbox('GeeksforGeeks');", "alert('GeeksforGeeks');"],
        answer: 3,
         
     },
     {
        question: "What is the correct syntax for referring to an external script called 'geek.js'?",
-       choices: ["<script src='geek.js'>", "<script href='geek.js'>", "<script ref='geek.js'>", "<script name='geek.js'>"],
+       choice: ["<script src='geek.js'>", "<script href='geek.js'>", "<script ref='geek.js'>", "<script name='geek.js'>"],
        answer: 0,
         
     },
     {
        question: "The external JavaScript file must contain <script> tag. True or False?",
-       choices: ["True", "False"],
-       answer: 0,
+       choice: ["True", "False"],
+       answer: 1,
         
     }
 ];
-// need to create a loop that will create li items based off of how many questions are in the array for choices
-for (let i = 0; i < questions.length; i++) {
-    questionTitle.textContent = (questions[0].question)
-    let createChoices = document.createElement("li");
-    createChoices.textContent = (questions[0].choices[i]);
-    choices.appendChild(createChoices);
-    createChoices.setAttribute("class", "list-group-item");
-};
-
-// // Store our li elements in a variable
-
-// let listItems = document.getElementsByTagName("li");
-
-// li1.textContent = "Chicken Fingers";
-// li2.textContent = "Pizza";
-// li3.textContent = "Burgers";
-// li4.textContent = "Sushi";
-
-// choices.appendChild(li1);
-// choices.appendChild(li2);
-// choices.appendChild(li3);
-// choices.appendChild(li4);
-
-// listItems[0].setAttribute("class", "list-group-item");
-// listItems[1].setAttribute("class", "list-group-item");
-// listItems[2].setAttribute("class", "list-group-item");
-// listItems[3].setAttribute("class", "list-group-item");
 
 // ***** Event Listeners ****
+// event listener for start button
+startBtn.addEventListener("click", startQuiz);
+// quizChoices.addEventListener("click", createQuestions);
+
+//event listener for quiz questions
 
 
 // ***** Functions *****
 // When I click on "Start quiz" I will see the first question and the timer will begin
-
-startQuiz.addEventListener("click", function() {
+function startQuiz() {
     console.log("You clicked start");
     quiz.removeChild(startCard);
-    // show question one
-    // start timer
-});
+    createQuestion();
+};
+
+console.log(questionItem[displayedQuestion].question);
+
+function createQuestion() {
+    // updates h5 tag with the title of the question and updates the class
+    questionTitle.textContent = questionItem[displayedQuestion].question;
+    quizCard.prepend(questionTitle);
+    questionTitle.classList.add("card-header");
+    // created ul for the li questions and updates the classes
+    let choiceWrapper = document.createElement("ul");
+    choiceWrapper.setAttribute("value", [choiceValue]);
+    quizCard.appendChild(choiceWrapper);
+    choiceWrapper.classList.add("list-group", "list-group-flush");
+    choiceWrapper.setAttribute("id", "quizChoices")
+
+    for (let j = 0; j < questionItem[displayedQuestion].choice.length; j++) {
+        let questionChoice = document.createElement("li");
+        questionChoice.textContent = questionItem[displayedQuestion].choice[j];
+        questionChoice.setAttribute("value", [choiceValue]);
+        choiceWrapper.appendChild(questionChoice);
+        questionChoice.classList.add("list-group-item");
+        choiceValue++;
+    };
+    displayedQuestion++;
+};
+
+    // create a title for the question
+    // display the choices below it
+    // allow the user to click on one of the choices
+    // display if the answer is incorrect or correct
+    // create and display next title and choices for the question
+        
+
+    // for (let j = 0; j < questionItem[i].choices.length; j++) {
+    //     
+
+    //     k++;
+    //     console.log(k);
+    //     console.log(questionItem[i].choices[j]);
+
+    //     if (k > questionItem[i].choices[j]) {
+    //         console.log("it is bigger");
+    //     }
+    // }
+
+    // when the player answers the question add i++
+
+
+
 
 // add click listening to all buttons to go to the next question. Wrong answers also change background color to red and correct answer to green. Can use li:active and the style of background color
 
